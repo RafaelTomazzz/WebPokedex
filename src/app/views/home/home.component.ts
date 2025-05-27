8d0f336bc4585634d69233c2c749de166ba750a7
RafaelTomazzz <125613679+RafaelTomazzz@users.noreply.github.com>
2025-05-27 02:45:48 -0300

    ApareuOsElementos

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
 import { Pokemon } from '../../models/pokemon';
 import { CommonModule } from '@angular/common';
+import { Elemento } from '../../enums/elemento'
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent implements OnInit{
   pokemons: Pokemon[]
+  elemento: Elemento
 
   constructor(public pokemonService: PokemonService) {}
 
@@ -24,5 +26,27 @@ export class HomeComponent implements OnInit{
 
       console.log(this.pokemons)
     })
+  }
+
+  currentType(type: number){
+    let elemento
+
+    switch(type){
+      case 1:
+        elemento = "normal"
+        break
+      case 2:
+        elemento = "fogo"
+        break
+      case 3:
+        elemento = "agua"
+        break
+      case 4:
+        elemento = "grama"
+        break
+    }
+    
+    console.log(elemento)
+    return elemento
   }
 }
